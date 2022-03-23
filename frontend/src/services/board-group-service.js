3b77512165a26ee9393c4b32a0702f1d07fc8d84
Inbar <inbar.tzvik@gmail.com>
2022-03-23 19:46:11 +0200

    edit group

--- frontend/src/services/board-group-service.js
+++ frontend/src/services/board-group-service.js
@@ -39,79 +39,12 @@ const board = [
         _id: 'u101',
         fullname: 'Tal Tarablus',
         imgUrl: 'https://www.google.com',
-    },
-    {
+      },
+      {
         _id: 'u102',
         fullname: 'Tl rablus',
         imgUrl: 'https://www.google.com',
-    },],
-    groups: [{
-            id: 'g101',
-            title: 'Group 1',
-            tasks: [{
-                    id: 'c101',
-                    title: 'Replace logo',
-                },
-                {
-                    id: 'c102',
-                    title: 'Add Samples',
-                },
-            ],
-            style: {},
-        },
-        {
-            id: 'g102',
-            title: 'Group 2',
-            tasks: [{
-                    id: 'c103',
-                    title: 'Do that',
-                },
-                {
-                    id: 'c104',
-                    title: 'Help me',
-                    status: 'in-progress',
-                    description: 'description',
-                    comments: [{
-                        id: 'ZdPnm',
-                        txt: 'also @yaronb please CR this',
-                        createdAt: 1590999817436.0,
-                        byMember: {
-                            _id: 'u101',
-                            fullname: 'Tal Tarablus',
-                            imgUrl: 'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
-                        },
-                    }, ],
-                    checklists: [{
-                        id: 'YEhmF',
-                        title: 'Checklist',
-                        todos: [{
-                            id: '212jX',
-                            title: 'To Do 1',
-                            isDone: false,
-                        }, ],
-                    }, ],
-                    members: [{
-                        _id: 'u101',
-                        username: 'Tal',
-                        fullname: 'Tal Tarablus',
-                        imgUrl: 'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
-                    }, ],
-                    labelIds: ['l101', 'l102'],
-                    createdAt: 1590999730348,
-                    dueDate: 16156215211,
-                    byMember: {
-                        _id: 'u101',
-                        username: 'Tal',
-                        fullname: 'Tal Tarablus',
-                        imgUrl: 'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
-                    },
-                    style: {
-                        bgColor: '#26de81',
-                    },
-                },
-            ],
-            style: {},
-        },
+      },
     ],
     groups: [
       {
@@ -127,7 +60,7 @@ const board = [
             title: 'Add Samples',
           },
         ],
-        style: {},
+        style: { bgColor: '#00FFFF' },
       },
       {
         id: 'g102',
@@ -189,7 +122,86 @@ const board = [
             },
           },
         ],
-        style: {},
+        style: { bgColor: '#00FFFF' },
+      },
+    ],
+    groups: [
+      {
+        id: 'g101',
+        title: 'Group 1',
+        tasks: [
+          {
+            id: 'c101',
+            title: 'Replace logo',
+          },
+          {
+            id: 'c102',
+            title: 'Add Samples',
+          },
+        ],
+        style: { bgColor: '#00FFFF' },
+      },
+      {
+        id: 'g102',
+        title: 'Group 2',
+        tasks: [
+          {
+            id: 'c103',
+            title: 'Do that',
+          },
+          {
+            id: 'c104',
+            title: 'Help me',
+            status: 'in-progress',
+            description: 'description',
+            comments: [
+              {
+                id: 'ZdPnm',
+                txt: 'also @yaronb please CR this',
+                createdAt: 1590999817436.0,
+                byMember: {
+                  _id: 'u101',
+                  fullname: 'Tal Tarablus',
+                  imgUrl: 'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+                },
+              },
+            ],
+            checklists: [
+              {
+                id: 'YEhmF',
+                title: 'Checklist',
+                todos: [
+                  {
+                    id: '212jX',
+                    title: 'To Do 1',
+                    isDone: false,
+                  },
+                ],
+              },
+            ],
+            members: [
+              {
+                _id: 'u101',
+                username: 'Tal',
+                fullname: 'Tal Tarablus',
+                imgUrl: 'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+              },
+            ],
+            labelIds: ['l101', 'l102'],
+            createdAt: 1590999730348,
+            dueDate: 16156215211,
+            byMember: {
+              _id: 'u101',
+              username: 'Tal',
+              fullname: 'Tal Tarablus',
+              imgUrl: 'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+            },
+            style: {
+              bgColor: '#26de81',
+            },
+          },
+        ],
+        style: { bgColor: 'red' },
       },
     ],
     activitiesLog: [
