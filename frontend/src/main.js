8d00442ec35c22b60db8b4e4da0998f4cf90ab9d
Itzik Vaknin <itzikv3@gmail.com>
2022-03-26 14:54:14 +0300

    halsofi

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,25 +7,25 @@ import store from './store';
 // Styles
 import '../styles/styles.scss';
 
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fas } from '@fortawesome/free-solid-svg-icons';
-// library.add(fas);
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-// library.add(fab);
-// import { far } from '@fortawesome/free-regular-svg-icons';
-// library.add(far);
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
 // // import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 // // library.add(faCaretDown);
 // // import { faPhone } from '@fortawesome/free-solid-svg-icons';
-// import { dom } from '@fortawesome/fontawesome-svg-core';
-// dom.watch();
+import { dom } from '@fortawesome/fontawesome-svg-core';
+dom.watch();
 // library.add(faUserSecret, faCaretDown)
 // library.add(faPhone);
 
 const app = createApp(App);
 
-// app.component('font-awesome-icon', FontAwesomeIcon);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(ElementPlus);
 app.use(router);
 app.use(store);
