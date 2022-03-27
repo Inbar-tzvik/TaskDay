a469660a71e7945073c07b376337d53e5165e90a
Itzik Vaknin <itzikv3@gmail.com>
2022-03-27 17:32:31 +0300

    sss

--- frontend/src/components/avatar-cmp.vue
+++ frontend/src/components/avatar-cmp.vue
@@ -2,15 +2,15 @@
   <section class="avatar">
   <!-- {{this.currUser}} -->
     <div class="demo-type">
-      <!-- <div>
+      <div>
         <el-avatar :icon="UserFilled" />
       </div>
       <div>
         <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
-      </div> -->
+      </div>
       <!-- <div v-for="val in vals" :key="val"> -->
         <div>
-        <!-- <el-avatar>{{this.val}}</el-avatar> -->
+        <el-avatar> user </el-avatar>
       </div>
     </div>
   </section>
@@ -40,16 +40,3 @@ currUser:null
 </script>
 
 
-<style scoped>
-.demo-type {
-  display: flex;
-}
-.demo-type > div {
-  flex: 1;
-  text-align: center;
-}
-
-.demo-type > div:not(:last-child) {
-  border-right: 1px solid var(--el-border-color);
-}
-</style>
