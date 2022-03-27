89bab282957fb3c1b40d78a34bde062c48bf9733
Itzik Vaknin <itzikv3@gmail.com>
2022-03-27 16:24:21 +0300

    shiva

---
+++ frontend/src/components/avatar-cmp.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="avatar">
+  <!-- {{this.currUser}} -->
+    <div class="demo-type">
+      <!-- <div>
+        <el-avatar :icon="UserFilled" />
+      </div>
+      <div>
+        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
+      </div> -->
+      <!-- <div v-for="val in vals" :key="val"> -->
+        <div>
+        <!-- <el-avatar>{{this.val}}</el-avatar> -->
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { UserFilled } from '@element-plus/icons-vue';
+
+export default {
+  props: ['val'], // Menu title from the parent
+  data() {
+    return {
+      boards: [],
+currUser:null
+    };
+  },
+  created() {
+    this.currUser = val
+    //     user() {
+    //   this.boards = this.$store.getters.boards;
+    // },
+  },
+  methods: {
+
+  },
+};
+</script>
+
+
+<style scoped>
+.demo-type {
+  display: flex;
+}
+.demo-type > div {
+  flex: 1;
+  text-align: center;
+}
+
+.demo-type > div:not(:last-child) {
+  border-right: 1px solid var(--el-border-color);
+}
+</style>
--- frontend/src/components/member-picker.vue
+++ frontend/src/components/member-picker.vue
@@ -1,12 +1,28 @@
 <template>
   <section class="dropDownMenuWrapper">
-    <button class="dropDownMenuButton" ref="menu" @click="openClose">{{ vals }}</button>
+    <button
+      
+      class="dropDownMenuButton"
+      ref="menu"
+      @click="openClose"
+    >
+      <!-- {{ `${imgUrl}` }} -->
+      <div v-for="imgUrl in membersImg"
+      :key="imgUrl" class="avatars">
+        <div>
+        <el-avatar size="small"  :src="imgUrl" />
+      </div>
+        <!-- <div>
+          <el-avatar>{{ val }}</el-avatar>
+        </div> -->
+      </div>
+    </button>
 
     <!-- <div class="iconWrapper">
       <div class="bar1" :class="{ 'bar1--open': isOpen }" />
       <div class="bar2" :class="{ 'bar2--open': isOpen }" />
       <div class="bar3" :class="{ 'bar3--open': isOpen }" />
-    </div> -->
+    </div>-->
 
     <section class="dropdownMenu" v-if="isOpen">
       <div class="menuArrow" />
@@ -18,19 +34,25 @@
 </template>
 
 <script>
+//ITZIK
+// import avatar from "./avatar-cmp.vue";
+import { UserFilled } from '@element-plus/icons-vue';
+
+
 export default {
   props: {
     task: Object,
   }, // Menu title from the parent
   data() {
     return {
-      boards:[],
-            opts: this.$store.getters.boards[0].members.map(member=>member.fullname),
-      vals: this.task.members.map(member=>member.fullname),
+      boards: [],
+      opts: this.$store.getters.boards[0].members.map(member => member.fullname),
+      membersFullname: this.task.members.map(member => member.fullname),
+      membersImg:this.task.members.map(member => member.imgUrl),
       isOpen: false, // Variable if the menu is open or closed
     };
   },
-  created(){
+  created() {
     //     user() {
     //   this.boards = this.$store.getters.boards;
     // },
@@ -41,7 +63,27 @@ export default {
       this.isOpen = !this.isOpen;
     },
   },
+  components: {
+    // avatar
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.avatars{
+
+  display: flex;
+}
+.avatar-img {
+  width: 27px;
+  height:24px;
+}
+// .demo-type > div {
+//   flex: 1;
+//   text-align: center;
+// }
+
+// .demo-type > div:not(:last-child) {
+//   border-right: 1px solid var(--el-border-color);
+// }
+</style>
