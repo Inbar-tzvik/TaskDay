26a5ff24a94b274e79bfaf767ca6dbe1311872ca
Itzik Vaknin <itzikv3@gmail.com>
2022-04-04 10:59:53 +0300

    shivaboom

--- frontend/src/components/board-header-main.vue
+++ frontend/src/components/board-header-main.vue
@@ -56,8 +56,7 @@
       </div>
     </div>
     <p class="subtitle" v-if="isInfo">
-      Check out the following items to learn about all the cool features and tools <a href="/main">TaskDay.com</a> has for you
-      :-)
+      Learn how to create tasks that are clear, transparent, and on point.
     </p>
   </section>
 </template>
